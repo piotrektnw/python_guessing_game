f2e1619c15d2a852256cf39cba6a7bdbdd072439
piotrektnw <grafomat@o2.pl>
2020-03-22 23:54:15 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,21 @@
-test = input("Test: ")
+secret_word = "castle" 
+guess = "" 
+guess_count = 0
+guess_limit = 3
+guess_limit_reached = False
 
+hint = input("For help type 'help': ")
+
+if hint.upper() == "HELP": 
+  print("Word we are looking for is a name of a kind of a building")
+
+while guess != secret_word and not(guess_limit_reached):
+  if guess_count < guess_limit: 
+    guess = input("Guess the secret word: ") 
+    guess_count += 1 
+    else: guess_limit_reached = True
+
+if guess_limit_reached: 
+  print("You lost!") 
+  else: 
+    print("You win!")
