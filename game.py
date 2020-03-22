e92e0e84b5ac7bb99344be12a7fed8e6362da522
piotrektnw <grafomat@o2.pl>
2020-03-23 00:36:30 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,21 +1,26 @@
-secret_word = "castle" 
+secret_word = "CASTLE" 
 guess = "" 
 guess_count = 0
 guess_limit = 3
 guess_limit_reached = False
 
-hint = input("For help type 'help': ")
+hints = ["Hint: It is made of stone", "Hint: Nowadays you visit it as a tourist", "Hint: It used to be a place of living for a kings", ""]
+hint_count = 0
 
+hint = input("Hi! Welcome to my first Python guessing game. For help type 'help': ")
 if hint.upper() == "HELP": 
   print("Word we are looking for is a name of a kind of a building")
 
-while guess != secret_word and not(guess_limit_reached):
+while guess.upper() != secret_word and not(guess_limit_reached):
   if guess_count < guess_limit: 
     guess = input("Guess the secret word: ") 
     guess_count += 1 
-    else: guess_limit_reached = True
+    print(hints[hint_count]
+    hint_count += 1
+   else: 
+     guess_limit_reached = True
 
 if guess_limit_reached: 
   print("You lost!") 
-  else: 
-    print("You win!")
+else: 
+  print("You win!")
